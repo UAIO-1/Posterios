e47f2d254c61ccea0724fbcd70bcd9d4cf75014e
YOSUA <uamosi36@gmail.com>
2022-01-03 20:18:07 +0700

    baru ini

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,7 +20,7 @@
                 background-color: #f5f5fa;
                 test bang ok sipp;
 
-                asdaw
+                asdawaspdiojpasdaspdjasjfapsfjapsfaf
             }
 
            .container2 {
